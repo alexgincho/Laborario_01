8c0d37e0dda0cb660c5124bb0a380b1e0386f880
Alexander Ch. G <ginchoalexch@gmail.com>
2021-08-28 23:14:52 -0500

    Conexion con la base de datos.

---
+++ Laborario_01/Models/Data/Db_Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laborario_01.Models.Data
+{
+    public class Db_Context: IdentityDbContext
+    {
+        public Db_Context(DbContextOptions<Db_Context> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Producto> productos { get; set; }
+    }
+}
